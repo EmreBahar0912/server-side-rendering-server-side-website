71bb419fe7b7d2dc7e48547bd5f3f16c19ee39bd
EmreBahar0912 <e.bahar906@gmail.com>
2026-03-04 21:41:18 +0100

    GET method nieuwspagina aangepast #2
    Nieuwe parameters toegevoegd en naamgeving veranderd.

--- server.js
+++ server.js
@@ -41,16 +41,17 @@ app.get('/', async function (request, response) {
    // Render index.liquid uit de Views map
    // Geef hier eventueel data aan mee
    const params = {
-    'filter[district]': 'algemeen'
+    'filter[district]': 'algemeen',
+    'fields': 'title, intro, date, cover.*'
   }
 
   const apiURL = 'https://fdnd-agency.directus.app/items/buurtcampuskrant_stories?' + new URLSearchParams(params)
   // console.log(apiURL)
 
-  const personResponse = await fetch(apiURL)
-  const personResponseJSON = await personResponse.json()
+  const apiResponse = await fetch(apiURL)
+  const apiResponseJSON = await apiResponse.json()
   // console.log(personResponseJSON.data)
-   response.render('index.liquid')
+   response.render('index.liquid', {stories: apiResponseJSON.data})
 })
 
 // Maak een POST route voor de index; hiermee kun je bijvoorbeeld formulieren afvangen

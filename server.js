80103a1995b647fa6cd7cc9f30547fa27e02a5be
EmreBahar0912 <e.bahar906@gmail.com>
2026-03-05 23:03:14 +0100

    GET route aangemaakt + filters Nieuw-West #3
    Nieuwe route aangemaakt voor Nieuw-West pagina met de GET method. Met daarbij de benodigde filters.

--- server.js
+++ server.js
@@ -54,6 +54,23 @@ app.get('/', async function (request, response) {
    response.render('index.liquid', {stories: apiResponseJSON.data})
 })
 
+app.get('/nieuw-west', async function (request, response) {
+   // Render index.liquid uit de Views map
+   // Geef hier eventueel data aan mee
+   const params = {
+    'filter[district]': 'nieuw-west',
+    'fields': 'title, intro, date, cover.id'
+  }
+
+  const apiURL = 'https://fdnd-agency.directus.app/items/buurtcampuskrant_stories?' + new URLSearchParams(params)
+  // console.log(apiURL)
+
+  const apiResponse = await fetch(apiURL)
+  const apiResponseJSON = await apiResponse.json()
+  // console.log(personResponseJSON.data)
+   response.render('nieuw-west.liquid', {stories: apiResponseJSON.data})
+})
+
 // Maak een POST route voor de index; hiermee kun je bijvoorbeeld formulieren afvangen
 // Hier doen we nu nog niets mee, maar je kunt er mee spelen als je wilt
 app.post('/', async function (request, response) {
